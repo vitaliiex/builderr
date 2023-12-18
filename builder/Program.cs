2264e54ccf4a1fed18145fedcae49d63075b1e3e
vitaliiex <151929039+vitaliiex@users.noreply.github.com>
2023-12-19 00:49:13 +0300

    builder

--- builder/Program.cs
+++ builder/Program.cs
@@ -1,10 +1,61 @@
 ﻿namespace builder
 {
+    public class Laptop
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }  
+        public string OS { get; set; }
+        public int Price { get; set; }
+        private Laptop() { }
+        public class Builder
+        {
+            private Laptop laptop = new Laptop();
+
+            public Builder WithName(string name)
+            {
+                laptop.Name = name;
+                return this;
+            }
+            public Builder WithModel(string model)
+            {
+                laptop.Model = model;
+                return this;
+            }
+            public Builder WithYear(int year)
+            {
+                laptop.Year = year;
+                return this;
+            }
+            public Builder WithOS(string os)
+            {
+                laptop.OS = os;
+                return this;
+            }
+            public Builder WithPrice(int price)
+            {
+                laptop.Price = price;
+                return this;
+            }
+            public Laptop Build()
+            {
+                return laptop;
+            }
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Laptop laptop = new Laptop.Builder()
+            .WithName("Lenovo")
+            .WithModel("vfdfdbjbd2")
+            .WithYear(2019)
+            .WithOS("Windows")
+            .WithPrice(29900)
+            .Build();
+            
         }
     }
 }
